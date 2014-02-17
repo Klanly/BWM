d49cc7251ea23bfa25c2a6a48e1aa46954d662a4
yangmingzhe <miaodadao@gmail.com>
2014-02-17 14:29:37 +0800

    引入Common组件

--- protobuf.bat
+++ protobuf.bat
@@ -2,9 +2,10 @@
 pushd "%~dp0"
 
 set protogen="3Party\protobuf-net r668\ProtoGen\protogen.exe"
-for /f "tokens=* delims=" %%i in ('dir /b /s Assets\Scripts\Common\*.proto') do (
+for /f "tokens=* delims=" %%i in ('dir /b /s Common\*.proto') do (
 	echo %%i
 	%protogen% -i:"%%i" -o:"%%i.cs" -q
+	move /Y "%%i.cs" Assets\Scripts\Common\
 )
 
 pause
