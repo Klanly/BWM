7b0cb3c451d8786c0f76a7c9a568af64c66117ee
yangmingzhe <miaodadao@gmail.com>
2014-02-19 19:12:16 +0800

    创建WP8项目目录

---
+++ WP8/ShenZuo/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+using ShenZuo.Resources;
+
+namespace ShenZuo
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
