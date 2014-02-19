29ec3c2c8dd82c22bd63419254d0acb144062ea2
yangmingzhe <miaodadao@gmail.com>
2014-02-19 09:16:08 +0800

    Escape退出测试场景

---
+++ Assets/Scripts/GX/EscapeQuit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 键盘Escape退出游戏
+/// </summary>
+public class EscapeQuit : MonoBehaviour
+{
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+			Application.Quit();
+	}
+}
