b9ea47ff4cc1a0c6e2f15e7ab39361e69453bf1d
yangmingzhe <miaodadao@gmail.com>
2014-02-24 11:46:00 +0800

    自动设置屏幕分辨率为960*640

---
+++ Assets/Scripts/ScreenResolution.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenResolution : MonoBehaviour
+{
+	public int width = 960;
+	public int height = 640;
+
+	void Start()
+	{
+		var pc = Application.platform == RuntimePlatform.WindowsPlayer
+			|| Application.platform == RuntimePlatform.LinuxPlayer
+			|| Application.platform == RuntimePlatform.OSXPlayer;
+		Screen.SetResolution(960, 640, !pc);
+	}
+}
