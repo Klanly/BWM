8f61497db29e00f2859b2877e79d678ee57470f7
yunana <yunana@ztgame.com>
2014-02-28 19:43:19 +0800

    阻挡初步提交，还不能编辑

---
+++ Assets/Scripts/Editor/MapNavEditor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MapNav))]
+public class MapNavEditor : Editor {
+
+	private SerializedProperty gridWidth;
+	private SerializedProperty gridHeight;
+	private SerializedProperty gridXNum;
+	private SerializedProperty gridZNum;
+	private SerializedProperty showGrids;
+
+	void OnEnable()
+	{
+		gridWidth = serializedObject.FindProperty("gridWidth");
+		gridHeight = serializedObject.FindProperty("gridHeight");
+		gridXNum = serializedObject.FindProperty("gridXNum");
+		gridZNum = serializedObject.FindProperty("gridZNum");
+		showGrids = serializedObject.FindProperty("showGrids");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		EditorGUILayout.Space();
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(gridWidth, new GUIContent("gridWidth"));
+		EditorGUILayout.PropertyField(gridHeight, new GUIContent("gridHeight"));
+		EditorGUILayout.PropertyField(gridXNum, new GUIContent("gridXNum"));
+		EditorGUILayout.PropertyField(gridZNum, new GUIContent("gridZNum"));
+		EditorGUILayout.PropertyField(showGrids, new GUIContent("showGrids"));
+		serializedObject.ApplyModifiedProperties();
+
+		if (GUILayout.Button("Update"))
+		{
+			((MapNav)target).Reset();
+		}
+	}
+
+	public void OnSceneGUI () {
+		if(showGrids.boolValue)
+		{
+			int _gridZNum = gridZNum.intValue;
+			int _gridXNum = gridXNum.intValue;
+			float _gridWidth = gridWidth.floatValue;
+			float _gridHeight = gridHeight.floatValue;
+
+			for(int z = 0; z < _gridZNum; ++z)
+			{
+				for(int x = 0; x < _gridXNum; ++x)
+				{
+					Vector3[] verts = {new Vector3(x*_gridWidth, 0.5f, z*_gridHeight),
+						new Vector3(x*_gridWidth, 0.5f, (z+1)*_gridHeight),
+						new Vector3((x+1)*_gridWidth, 0.5f, (z+1)*_gridHeight),
+						new Vector3((x+1)*_gridWidth, 0.5f, z*_gridHeight)};
+
+					Handles.DrawSolidRectangleWithOutline(verts, new Color(1,1,1,0.2f), new Color(0,0,0,1));
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MapNav.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class MapNav : MonoBehaviour {
+
+	public float gridWidth = 1.0f;
+	public float gridHeight = 1.0f;
+	public int gridXNum = 0;
+	public int gridZNum = 0;
+	public bool showGrids = false;
+
+	public uint[] grids;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Creates a new grid of tile nodes of x by y count
+	/// </summary>
+	public void Reset()
+	{
+		grids = new uint[gridZNum * gridXNum];
+		for(int i = 0; i < grids.Length; ++i)
+			grids[i] = 0;
+	}
+
+	// get a grid at index of grids
+	public uint this[int _z, int _x]
+	{ 
+		get 
+		{
+			if(grids == null) return 0;
+			if(_z < 0 && _z > gridZNum) return 0;
+			if(_x < 0 && _x > gridXNum) return 0;
+			return grids[_z * gridZNum + _x];
+		} 
+
+		set
+		{
+			if(grids == null) return;
+			if(_z < 0 && _z > gridZNum) return;
+			if(_x < 0 && _x > gridXNum) return;
+			grids[_z * gridZNum + _x] = value;
+		}
+	}
+	
+	// shortcut to getting the length of grids.
+	public int Length 
+	{ 
+		get 
+		{
+			if (grids == null) return 0;
+			return grids.Length; 
+		} 
+	}
+
+
+}
