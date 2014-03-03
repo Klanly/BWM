e585c247bd16265e638e7bf1204e216771bafd98
yunana <yunana@ztgame.com>
2014-03-03 23:00:46 +0800

    阻挡插件

--- Assets/Scripts/MapNav.cs
+++ Assets/Scripts/MapNav.cs
@@ -9,8 +9,34 @@ public class MapNav : MonoBehaviour {
 	public int gridXNum = 0;
 	public int gridZNum = 0;
 	public bool showGrids = false;
-
 	public uint[] grids;
+
+	// 格子类型，0表示都不能走，每位0表示不可走，1表示可以走
+	public enum TileType
+	{
+		Walk	= 0x1,	// 可以行走
+		Water	= 0x2,	// 可以游泳
+	}
+	public TileType curTileType = TileType.Walk;			// 当前修改的阻挡标志
+
+	// 格子颜色
+	public static Color[] TileColor = 
+	{
+		Color.green,	// walk
+		Color.cyan,		// water
+		// etc
+		// etc
+	};
+
+	// 操作类型
+	public enum ProcessType
+	{
+		None 	= 0,	// 没有操作
+		Set		= 1,	// 设置
+		Clear	= 2,	// 清空
+	}
+	public ProcessType curProcessType = ProcessType.None;	// 当前操作类型
+	public int radius = 1;									// 操作半径
 
 
 	// Use this for initialization
@@ -34,22 +60,22 @@ public class MapNav : MonoBehaviour {
 	}
 
 	// get a grid at index of grids
-	public uint this[int _z, int _x]
+	public uint this[int _x, int _z]
 	{ 
 		get 
 		{
 			if(grids == null) return 0;
-			if(_z < 0 && _z > gridZNum) return 0;
-			if(_x < 0 && _x > gridXNum) return 0;
-			return grids[_z * gridZNum + _x];
+			if(_z < 0 && _z >= gridZNum) return 0;
+			if(_x < 0 && _x >= gridXNum) return 0;
+			return grids[_z * gridXNum + _x];
 		} 
 
 		set
 		{
 			if(grids == null) return;
-			if(_z < 0 && _z > gridZNum) return;
-			if(_x < 0 && _x > gridXNum) return;
-			grids[_z * gridZNum + _x] = value;
+			if(_z < 0 && _z >= gridZNum) return;
+			if(_x < 0 && _x >= gridXNum) return;
+			grids[_z * gridXNum + _x] = value;
 		}
 	}
 	
@@ -63,10 +89,30 @@ public class MapNav : MonoBehaviour {
 		} 
 	}
 
-	void OnDrawGizmos() {
+	public int getX(Vector3 position)
+	{
+		return (int)(position.x / gridWidth);
+	}
+
+	public int getZ(Vector3 position)
+	{
+		return (int)(position.z / gridHeight);
+	}
+
+	public Vector3 getPosition(int x, int z)
+	{
+		if(x < 0) x = 0;
+		if(x > gridXNum-1) x = gridXNum - 1;
+		if(z < 0) z = 0;
+		if(z > gridZNum-1) z = gridZNum - 1;
+		return new Vector3((x + 0.5f) * gridWidth, 0.0f, (z + 0.5f) * gridHeight);
+	}
+
+	void OnDrawGizmos() 
+	{
 		if(showGrids)
 		{
-			float y = 0.2f;
+			float y = 0.1f;
 			for(int z = 0; z < gridZNum; ++z)
 			{
 				Gizmos.color = new Color(0, 0, 0, 0.5F);
@@ -83,10 +129,23 @@ public class MapNav : MonoBehaviour {
 			{
 				for(int x = 0; x < gridXNum; ++x)
 				{
-					Gizmos.color = new Color(0.5f, 0.5f, 0.5f, 0.5F);
+					uint flag = this[x,z];
+					if(flag == 0)
+						Gizmos.color = new Color(0.5f, 0.0f, 0.0f, 0.5f);
+					else
+						Gizmos.color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+
 					Vector3 center = new Vector3(x * gridWidth + gridWidth * 0.5f, y, z * gridHeight + gridHeight * 0.5f);
 					Vector3 size = new Vector3(gridWidth, 0, gridHeight);
 					Gizmos.DrawCube(center, size);
+
+					if((flag & (uint)TileType.Walk) > 0)
+					{
+						Gizmos.color = TileColor[0];
+						center = new Vector3(x * gridWidth + gridWidth * 0.5f - gridWidth * 0.25f, y, z * gridHeight + gridHeight * 0.5f - gridHeight * 0.25f);
+						size = new Vector3(gridWidth * 0.25f, 0, gridHeight * 0.25f);
+						Gizmos.DrawCube(center, size);
+					}
 				}
 			}
 		}
