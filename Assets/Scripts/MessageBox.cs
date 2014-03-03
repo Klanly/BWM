b85609526ba8ee89423acc81d7a2a43ae4292ba0
yangmingzhe <miaodadao@gmail.com>
2014-03-03 14:23:36 +0800

    MessageBox prefab

---
+++ Assets/Scripts/MessageBox.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MessageBox : MonoBehaviour
+{
+	private string title;
+	private string message;
+
+	public UILabel messageBoxTitle;
+	public UILabel messageBoxText;
+	public GameObject messageBoxOK;
+	public GameObject messageBoxCancel;
+
+	void Start()
+	{
+		Close();
+	}
+
+	public void Close(GameObject sender = null)
+	{
+		this.gameObject.SetActive(false);
+		UIEventListener.Get(this.messageBoxOK).onClick = null;
+		UIEventListener.Get(this.messageBoxCancel).onClick = null;
+	}
+
+	public void Show(string message, string title = null, UIEventListener.VoidDelegate onOK = null, UIEventListener.VoidDelegate onCancel = null)
+	{
+		this.gameObject.SetActive(true);
+
+		this.title = title ?? string.Empty;
+		this.message = message;
+		
+		onCancel = onCancel ?? Close;
+		onOK = onOK ?? onCancel;
+		UIEventListener.Get(this.messageBoxOK).onClick = onOK;
+		UIEventListener.Get(this.messageBoxCancel).onClick = onCancel;
+	}
+
+	void Update()
+	{
+		this.messageBoxTitle.text = title ?? string.Empty;
+		this.messageBoxText.text = message ?? string.Empty;
+	}
+}
