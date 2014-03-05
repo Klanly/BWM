48755357597aaf09a67bbcbb9f51e9b5efcd164d
yunana <yunana@ztgame.com>
2014-03-05 21:21:06 +0800

    初步提交角色创建界面

--- Assets/Scenes/RoleCreateScene.cs
+++ Assets/Scenes/RoleCreateScene.cs
@@ -6,9 +6,9 @@ using GX.Net;
 public class RoleCreateScene : MonoBehaviour
 {
 	public UIInput roleNameInput;
-	public UIButton okButton;
-
 	public bool sexman = true;
+	public int profession = 1;
+
 	private GameObject spriteZhanshi, spriteDaoshi, spriteFashi;
 
 	void btnMale_onClick(GameObject sender)
@@ -27,6 +27,7 @@ public class RoleCreateScene : MonoBehaviour
 
 	void btnZhanshi_onClick(GameObject sender)
 	{
+		profession = 1;
 		spriteZhanshi.SetActive(true);
 		spriteDaoshi.SetActive(false);
 		spriteFashi.SetActive(false);
@@ -38,6 +39,7 @@ public class RoleCreateScene : MonoBehaviour
 
 	void btnDaoshi_onClick(GameObject sender)
 	{
+		profession = 2;
 		spriteZhanshi.SetActive(false);
 		spriteDaoshi.SetActive(true);
 		spriteFashi.SetActive(false);
@@ -49,6 +51,7 @@ public class RoleCreateScene : MonoBehaviour
 
 	void btnFashi_onClick(GameObject sender)
 	{
+		profession = 3;
 		spriteZhanshi.SetActive(false);
 		spriteDaoshi.SetActive(false);
 		spriteFashi.SetActive(true);
@@ -65,6 +68,14 @@ public class RoleCreateScene : MonoBehaviour
 		roleNameInput.value = name;
 	}
 
+	void btnOK_onClick(GameObject sender)
+	{
+		Net.Instance.Send(new CheckCharNameSelectUserCmd_CS()
+        {
+			charname = roleNameInput.value,
+		});
+	}
+
 	void Start()
 	{
 		UIEventListener.Get(GameObject.Find("btnMale")).onClick = this.btnMale_onClick;
@@ -73,6 +84,7 @@ public class RoleCreateScene : MonoBehaviour
 		UIEventListener.Get(GameObject.Find("btnDaoshi")).onClick = this.btnDaoshi_onClick;
 		UIEventListener.Get(GameObject.Find("btnFashi")).onClick = this.btnFashi_onClick;
 		UIEventListener.Get(GameObject.Find("btnSuiji")).onClick = this.btnSuiji_onClick;
+		UIEventListener.Get(GameObject.Find("btnOK")).onClick = this.btnOK_onClick;
 
 		spriteZhanshi = GameObject.Find("spriteZhanshi");
 		spriteDaoshi = GameObject.Find("spriteDaoshi");
@@ -82,13 +94,12 @@ public class RoleCreateScene : MonoBehaviour
 		spriteFashi.SetActive(false);
 
 		// 随机玩家名
+		roleNameInput = GameObject.Find("inputRoleName").GetComponent<UIInput>();
 		btnSuiji_onClick(null);
 
-		//roleNameInput.isSelected = true; // focus it
-		UIEventListener.Get(okButton.gameObject).onClick = go => Net.Instance.Send(new CheckCharNameSelectUserCmd_CS() 
-		{
-			charname = roleNameInput.value,
-		});
+		// 初始选择战士、女性
+		btnZhanshi_onClick(null);
+		btnFemale_onClick(null);
 	}
 
 
@@ -106,7 +117,8 @@ public class RoleCreateScene : MonoBehaviour
 			Net.Instance.Send(new CharactorCreateSelectUserCmd_C()
 			{
 				charname = cmd.charname,
-				//sexman = my.sexman,
+				sexman = my.sexman,
+				//profession = my.profession;
 			});
 		}
 	}
