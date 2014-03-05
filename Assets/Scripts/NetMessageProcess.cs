c3f4fcd1574ac76a519e29356575cbc6cd02c4a1
yangmingzhe <miaodadao@gmail.com>
2014-03-05 17:54:15 +0800

    全局网络消息处理放在NetMessageProcess中

---
+++ Assets/Scripts/NetMessageProcess.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using GX.Net;
+using Cmd;
+
+/// <summary>
+/// 全局网络消息处理
+/// 和具体业务相关的消息处理请直接放在其实现文件中，以保持信息的局部性
+/// </summary>
+public static class NetMessageProcess
+{
+	[Execute]
+	static void Execute(MessageBoxChatUserCmd_S cmd)
+	{
+		MessageBox.Show(cmd.info);
+	}
+}
