c596e78f60b2417f5e7452e6ed0e6e776c1b2450
yunana <yunana@ztgame.com>
2014-03-05 23:19:49 +0800

    初步提交角色列表

--- Assets/Scenes/RoleListScene.cs
+++ Assets/Scenes/RoleListScene.cs
@@ -5,43 +5,65 @@ using GX.Net;
 
 public class RoleListScene : MonoBehaviour
 {
-	public UIGrid roleList;
-	public GameObject roleItem;
-	public UIButton roleCreate;
+	public static int s_maxRoleNum = 3;
+	private GameObject btnRole;
+	private GameObject btnRoleCreate;
+	private string[] spriteNameProfession = {"button_zhanshi", "button_daoshi", "button_fashi"};
 
 	// Use this for initialization
 	void Start()
 	{
-		UIEventListener.Get(roleCreate.gameObject).onClick = go => Application.LoadLevel("RoleCreateScene");
+		btnRole = GameObject.Find("btnRole");
+		btnRoleCreate = GameObject.Find("btnRoleCreate");
+		btnRole.SetActive(false);
+		btnRoleCreate.SetActive(false);
 	}
 
 	private void ShowRoleList(CharactorListReturnSelectUserCmd_S cmd)
 	{
-		roleCreate.isEnabled = cmd.list.Count < 5;
-
-		foreach (Transform t in roleList.transform)
+		var gridRoleList = GameObject.Find("gridRoleList").GetComponent<UIGrid>();
+		foreach (Transform t in gridRoleList.transform)
 			DestroyObject(t.gameObject);
-		roleList.Reposition();
+
+		var num = cmd.list.Count;
+		if(num < s_maxRoleNum)
+			num += 1;
+		var height = gridRoleList.cellHeight * num - (gridRoleList.cellHeight - btnRole.GetComponent<UISprite>().height);
+		var oldPosition = gridRoleList.transform.localPosition;
+		oldPosition.y = height / 2.0f;
+		gridRoleList.transform.localPosition = oldPosition;
+		gridRoleList.Reposition();
 
 		foreach (var role in cmd.list)
 		{
-			var item = Instantiate(roleItem) as GameObject;
-			item.transform.parent = roleList.transform;
+			var item = Instantiate(btnRole) as GameObject;
+			item.transform.parent = gridRoleList.transform;
 			item.transform.localScale = Vector3.one;
 
 			var info = role;
-			item.transform.Find("Name").GetComponent<UILabel>().text = info.charname;
-			UIEventListener.Get(item.transform.Find("Login").gameObject).onClick = go => Net.Instance.Send(new CharactorSelectSelectUserCmd_C()
+			item.transform.Find("labelName").GetComponent<UILabel>().text = info.charname;
+			item.transform.Find("labelLevel").GetComponent<UILabel>().text = "LV" + 10;
+			item.transform.Find("spriteProfession").GetComponent<UISprite>().spriteName = spriteNameProfession[1];
+			UIEventListener.Get(item.transform.Find("btnRole").gameObject).onClick = go => Net.Instance.Send(new CharactorSelectSelectUserCmd_C()
 			{
 				charid = info.charid,
 			});
 
-			UIEventListener.Get(item.transform.Find("Delete").gameObject).onClick = go => Net.Instance.Send(new CharactorDeleteSelectUserCmd_C()
+			UIEventListener.Get(item.transform.Find("btnDelete").gameObject).onClick = go => Net.Instance.Send(new CharactorDeleteSelectUserCmd_C()
 			{
 				charid = info.charid,
 			});
 		}
-		roleList.Reposition();
+
+		if(cmd.list.Count < s_maxRoleNum)
+		{
+			var item = Instantiate(btnRoleCreate) as GameObject;
+			item.transform.parent = gridRoleList.transform;
+			item.transform.localScale = Vector3.one;
+			UIEventListener.Get(item.transform.Find("btnRoleCreate").gameObject).onClick = go => Application.LoadLevel("RoleCreateScene");
+		}
+
+		gridRoleList.Reposition();
 	}
 
 
