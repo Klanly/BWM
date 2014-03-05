939e38f0091e8c697f462e6146d8e14417433356
yangmingzhe <miaodadao@gmail.com>
2014-03-05 17:57:10 +0800

    响应服务器时间检查消息

--- Assets/Scripts/NetMessageProcess.cs
+++ Assets/Scripts/NetMessageProcess.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using GX.Net;
 using Cmd;
+using System;
 
 /// <summary>
 /// 全局网络消息处理
@@ -9,9 +10,23 @@ using Cmd;
 /// </summary>
 public static class NetMessageProcess
 {
+	/// <summary>
+	/// 全局MessageBox
+	/// </summary>
+	/// <param name="cmd"></param>
 	[Execute]
 	static void Execute(MessageBoxChatUserCmd_S cmd)
 	{
 		MessageBox.Show(cmd.info);
+	}
+
+	/// <summary>
+	/// 服务器对客户端的时间探测，可用于反外挂等时间相关业务
+	/// </summary>
+	/// <param name="cmd"></param>
+	[Execute]
+	static void Execute(TickRequestNullUserCmd_CS cmd)
+	{
+		Net.Instance.Send(new TickReturnNullUserCmd_CS() { requesttime = cmd.requesttime, mytime = DateTime.Now.ToUnixTime() });
 	}
 }
