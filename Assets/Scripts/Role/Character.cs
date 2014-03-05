18788d48c064fcfff43a95012c82785947fafdd0
yangmingzhe <miaodadao@gmail.com>
2014-03-05 18:37:19 +0800

    角色信息的简单处理

---
+++ Assets/Scripts/Role/Character.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using GX.Net;
+using Cmd;
+using System.Collections.Generic;
+using CharacterInfo = Cmd.AddMapUserDataAndPosMapUserCmd_S;
+
+public class Character : MonoBehaviour
+{
+	public static Dictionary<ulong, CharacterInfo> All { get; private set; }
+
+	static Character()
+	{
+		All = new Dictionary<ulong, CharacterInfo>();
+	}
+
+	[Execute]
+	static void Execute(CharacterInfo cmd)
+	{
+		if (Character.All.ContainsKey(cmd.data.charid) == false)
+		{
+			// TODO: add charater to scene
+		}
+		Character.All[cmd.data.charid] = cmd;
+	}
+
+	[Execute]
+	static void Execute(UserMoveDownMoveUserCmd_S cmd)
+	{
+		CharacterInfo info;
+		if (Character.All.TryGetValue(cmd.charid, out info))
+			info.pos = cmd.pos;
+	}
+
+	[Execute]
+	static void Execute(UserGotoMoveUserCmd_S cmd)
+	{
+		CharacterInfo info;
+		if (Character.All.TryGetValue(cmd.charid, out info))
+		{
+			info.pos = cmd.pos;
+			// TODO: process cmd.mapid
+		}
+	}
+}
---
+++ Assets/Scripts/Role/MainCharacter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cmd;
+using GX.Net;
+using GX;
+using MainCharacterInfo = Cmd.FirstMainUserDataAndPosMapUserCmd_S;
+
+public class MainCharacter : Singleton<MainCharacter>
+{
+	public static MainCharacterInfo ServerInfo { get; private set; }
+
+	static MainCharacter()
+	{
+		ServerInfo = new MainCharacterInfo(); // 避免不必要的空指针判断
+	}
+
+	/// <summary>
+	/// 更新主角信息
+	/// </summary>
+	/// <param name="cmd"></param>
+	[Execute]
+	static void Execute(MainCharacterInfo cmd)
+	{
+		ServerInfo = cmd;
+	}
+}
