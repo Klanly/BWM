4e0ceffe24fecac080d49f08eb5620989bcf4097
yangmingzhe <miaodadao@gmail.com>
2014-03-06 11:09:45 +0800

    角色列表可显示

--- Assets/Scenes/RoleListScene.cs
+++ Assets/Scenes/RoleListScene.cs
@@ -7,16 +7,13 @@ public class RoleListScene : MonoBehaviour
 {
 	public const int MaxRoleNum = 3;
 
-	private GameObject btnRole;
+	public GameObject btnRole;
 	public GameObject btnRoleCreate;
 	private string[] spriteNameProfession = {"button_zhanshi", "button_daoshi", "button_fashi"};
 
 	// Use this for initialization
 	void Start()
 	{
-		btnRole = GameObject.Find("btnRole");
-		btnRole.SetActive(false);
-
 		btnRoleCreate.SetActive(false);
 		UIEventListener.Get(btnRoleCreate).onClick = go => Application.LoadLevel("RoleCreateScene");
 	}
@@ -46,15 +43,11 @@ public class RoleListScene : MonoBehaviour
 			item.transform.Find("labelName").GetComponent<UILabel>().text = info.charname;
 			item.transform.Find("labelLevel").GetComponent<UILabel>().text = "LV" + 10;
 			item.transform.Find("spriteProfession").GetComponent<UISprite>().spriteName = spriteNameProfession[1];
-			UIEventListener.Get(item.gameObject).onClick = go => Net.Instance.Send(new CharactorSelectSelectUserCmd_C()
-			{
-				charid = info.charid,
-			});
+			UIEventListener.Get(item.gameObject).onClick = go =>
+				Net.Instance.Send(new CharactorSelectSelectUserCmd_C() { charid = info.charid, });
 
-			UIEventListener.Get(item.transform.Find("btnDelete").gameObject).onClick = go => Net.Instance.Send(new CharactorDeleteSelectUserCmd_C()
-			{
-				charid = info.charid,
-			});
+			UIEventListener.Get(item.transform.Find("btnDelete").gameObject).onClick = go =>
+				Net.Instance.Send(new CharactorDeleteSelectUserCmd_C() { charid = info.charid, });
 		}
 
 		btnRoleCreate.SetActive(cmd.list.Count < MaxRoleNum);
