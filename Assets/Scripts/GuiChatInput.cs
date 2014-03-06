64fb0ec5bc5d2289c09b032f35e2236fcc6786d3
yangmingzhe <miaodadao@gmail.com>
2014-03-06 13:52:16 +0800

    聊天消息可以发送和显示了

---
+++ Assets/Scripts/GuiChatInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cmd;
+using GX.Net;
+
+public class GuiChatInput : MonoBehaviour
+{
+	public UIButton sendButton;
+	public UIInput chatInput;
+
+	// Use this for initialization
+	void Start()
+	{
+		UIEventListener.Get(sendButton.gameObject).onClick = go => SendChat();
+	}
+
+	public void SendChat(string message = null)
+	{
+		if (message == null)
+		{
+			message = chatInput.value.Trim();
+			chatInput.value = null;
+		}
+
+		Net.Instance.Send(new CommonChatUserCmd_CS()
+		{
+			charid = MainCharacter.ServerInfo.data.charid,
+			chatpos = CommonChatUserCmd_CS.ChatPos.ChatPos_Normal,
+			info = message,
+		});
+	}
+
+	void Update()
+	{
+		sendButton.isEnabled = !string.IsNullOrEmpty(chatInput.value.Trim());
+		if (sendButton.isEnabled && Input.GetKeyDown(KeyCode.Return))
+			SendChat();
+	}
+}
