c9747bf13cae40fcaf3d92a484ae0acdb8489ec8
yangmingzhe <miaodadao@gmail.com>
2014-03-07 11:18:08 +0800

    fix: AutoLoginScene多次跳转

--- Assets/Scripts/AutoLoginScene.cs
+++ Assets/Scripts/AutoLoginScene.cs
@@ -6,14 +6,19 @@ using System.Collections;
 /// </summary>
 public class AutoLoginScene : MonoBehaviour
 {
+#if UNITY_EDITOR
+	static bool jumped = false;
 	void Start()
 	{
-#if UNITY_EDITOR
-		if (System.IO.Path.GetFileNameWithoutExtension(UnityEditor.EditorApplication.currentScene) != LoginScene.Name)
-		{
-			Application.LoadLevel(LoginScene.Name);
+		if (jumped)
 			return;
+		jumped = true;
+		var current = System.IO.Path.GetFileNameWithoutExtension(UnityEditor.EditorApplication.currentScene);
+		if (current != LoginScene.Name)
+		{
+			Debug.Log(string.Format("Auto jump: {0} -> {1}", current, LoginScene.Name));
+			Application.LoadLevel(LoginScene.Name);
 		}
-#endif
 	}
+#endif
 }
