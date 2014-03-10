a2ca24eebcb08dfd5cb2121023d99864dd63de9f
yangmingzhe <miaodadao@gmail.com>
2014-03-10 13:18:07 +0800

    Lua脚本可加载执行

---
+++ Assets/Tests/LuaTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LuaTest : MonoBehaviour
+{
+	string script = string.Empty;
+
+	void OnGUI()
+	{
+		script = GUI.TextArea(new Rect(10, 10, Screen.width - 20, Screen.height - 50), script);
+		if (GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height - 30, 80, 20), "OK"))
+		{
+			Run();
+		}
+	}
+
+	private bool Run()
+	{
+		try
+		{
+			Debug.Log("Run... \n" + script);
+			using (var lua = new NLua.Lua())
+			{
+				lua.LoadCLRPackage();
+				var returns = lua.DoString(script);
+				Debug.Log(returns);
+			}
+			Debug.Log("OK");
+			return true;
+		}
+		catch (Exception ex)
+		{
+			Debug.LogException(ex);
+			return false;
+		}
+	}
+}
