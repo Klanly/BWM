ae93646ba8d6d5e5175583428f046eb79ae20f5b
yangmingzhe <miaodadao@gmail.com>
2014-03-11 14:05:03 +0800

    GuiChatInput 和 GuiChatOutput 设计时隐藏

--- Assets/Scenes/BattleScene.cs
+++ Assets/Scenes/BattleScene.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class BattleScene : MonoBehaviour
 {
+	public GuiChatInput guiChatInput;
+	public GuiChatOutput guiChatOutput;
 
 	void Start()
 	{
+		guiChatInput.gameObject.SetActive(true);
+		guiChatOutput.gameObject.SetActive(true);
 	}
 }
