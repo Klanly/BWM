260520d307689e8c9ca4f070377f64a8c4ee6ef9
yangmingzhe <miaodadao@gmail.com>
2014-03-11 15:36:56 +0800

    主界面 长短聊天界面内容可切换

--- Assets/Scripts/GuiChatOutput.cs
+++ Assets/Scripts/GuiChatOutput.cs
@@ -3,18 +3,42 @@ using System.Collections;
 using GX.Net;
 using Cmd;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GuiChatOutput : MonoBehaviour
 {
 	public const int MaxChatLines = 10;
+	public const int MaxSnapshotLines = 2;
 
-	public UILabel chatMessage;
 	private readonly List<string> lines = new List<string>();
+
+	public GameObject content;
+	public UILabel contentChatMessage;
+	public UIButton contentToggle;
+
+	public GameObject snapshot;
+	public UILabel snapshotChatMessage;
+	public UIButton snapshotToggle;
 
 	void Start()
 	{
 		lines.Clear();
-		chatMessage.text = string.Empty;
+		contentChatMessage.text = string.Empty;
+		UIEventListener.Get(contentToggle.gameObject).onClick = Toggle;
+
+		snapshotChatMessage.text = string.Empty;
+		UIEventListener.Get(snapshotToggle.gameObject).onClick = Toggle;
+
+		Toggle();
+	}
+
+	void Toggle(GameObject sender = null)
+	{
+		var full = content.activeSelf;
+		content.SetActive(!full);
+		snapshot.SetActive(full);
+
+		NGUITools.BringForward(this.gameObject);
 	}
 
 	[Execute]
@@ -27,11 +51,13 @@ public class GuiChatOutput : MonoBehaviour
 		if (my.lines.Count > MaxChatLines)
 		{
 			my.lines.RemoveRange(0, my.lines.Count - MaxChatLines);
-			my.chatMessage.text = string.Join(string.Empty, my.lines.ToArray());
+			my.contentChatMessage.text = string.Join(string.Empty, my.lines.ToArray());
 		}
 		else
 		{
-			my.chatMessage.text += chat;
+			my.contentChatMessage.text += chat;
 		}
+
+		my.snapshotChatMessage.text = string.Join(string.Empty, my.lines.Skip(my.lines.Count - MaxSnapshotLines).ToArray());
 	}
 }
