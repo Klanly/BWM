5758865ea3834a1409d46e8b9d7cd4a5198db24c
yangmingzhe <miaodadao@gmail.com>
2014-03-11 14:43:16 +0800

    主界面 控制按钮

--- Assets/Scenes/BattleScene.cs
+++ Assets/Scenes/BattleScene.cs
@@ -3,12 +3,4 @@ using System.Collections;
 
 public class BattleScene : MonoBehaviour
 {
-	public GuiChatInput guiChatInput;
-	public GuiChatOutput guiChatOutput;
-
-	void Start()
-	{
-		guiChatInput.gameObject.SetActive(true);
-		guiChatOutput.gameObject.SetActive(true);
-	}
 }
