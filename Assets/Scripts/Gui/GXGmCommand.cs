9c683af61a8ef49f501909275d25c7ba50589b96
yangmingzhe <miaodadao@gmail.com>
2014-03-17 18:06:05 +0800

    GM命令列表 stub

---
+++ Assets/Scripts/Gui/GXGmCommand.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using GX.Net;
+using Cmd;
+using System.Collections.Generic;
+
+public class GXGmCommand : MonoBehaviour
+{
+	public GameObject itemTemplate;
+	public UIScrollView scrollView;
+
+	void SetValues(IEnumerable<GMHelpInfo> infos)
+	{
+		foreach (var i in infos)
+		{
+			Debug.LogError(i.method);
+		}
+	}
+
+	[Execute]
+	static void Execute(GMCommandListChatUserCmd_S cmd)
+	{
+		var my = BattleScene.Instance.Gui<GXGmCommand>();
+		my.SetValues(cmd.list);
+	}
+}
