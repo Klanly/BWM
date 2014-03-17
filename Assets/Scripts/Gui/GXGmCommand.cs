6d113f4b60ea50ecadbbe90bb26a2a024b555165
yangmingzhe <miaodadao@gmail.com>
2014-03-17 18:43:20 +0800

    GM命令列表 内容可简单显示

--- Assets/Scripts/Gui/GXGmCommand.cs
+++ Assets/Scripts/Gui/GXGmCommand.cs
@@ -11,10 +11,30 @@ public class GXGmCommand : MonoBehaviour
 
 	void SetValues(IEnumerable<GMHelpInfo> infos)
 	{
+		var canvas = scrollView.GetComponent<UIGrid>();
+
+		// remove all childer of canvas
+		foreach (Transform d in canvas.transform)
+			DestroyObject(d.gameObject);
+		canvas.Reposition();
+		scrollView.ResetPosition();
+
 		foreach (var i in infos)
 		{
-			Debug.LogError(i.method);
+			var info = i;
+			var item = GameObject.Instantiate(itemTemplate) as GameObject;
+			item.transform.parent = canvas.transform;
+			item.transform.localScale = Vector3.one;
+
+			item.GetComponentInChildren<UILabel>().text = info.method;
+			UIEventListener.Get(item.GetComponentInChildren<UIButton>().gameObject).onClick = go =>
+			{
+				Debug.Log(info.ToStringDebug());
+				BattleScene.Instance.Gui<GXChatInput>().chatInput.value = string.Format("// {0} {1}", info.method, info.example);
+			};
 		}
+
+		canvas.Reposition();
 	}
 
 	[Execute]
