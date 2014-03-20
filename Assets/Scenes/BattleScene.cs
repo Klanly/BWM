b2dcc137a0331b3aaf294214ab1f016963b56454
yangmingzhe <miaodadao@gmail.com>
2014-03-20 14:43:57 +0800

    BattleScene采用PrefabLoader加载GUI

--- Assets/Scenes/BattleScene.cs
+++ Assets/Scenes/BattleScene.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class BattleScene : MonoBehaviour
@@ -8,60 +9,14 @@ public class BattleScene : MonoBehaviour
 	public static BattleScene Instance { get; private set; }
 
 	#region GUI
-	public UIRoot uiRoot;
-	private readonly Dictionary<string, MonoBehaviour> guiCache = new Dictionary<string, MonoBehaviour>();
-
 	/// <summary>
-	/// 获取给定类型的GUI组件，会进行单件实例的缓存，以避免重复加载
+	/// 获取给定类型的GUI组件
 	/// </summary>
 	/// <typeparam name="T"></typeparam>
 	/// <returns>失败返回null</returns>
 	public T Gui<T>() where T : MonoBehaviour
 	{
-		MonoBehaviour cache;
-		if (guiCache.TryGetValue(typeof(T).FullName, out cache))
-			return (T)cache;
-
-		var go = LoadGui(typeof(T).Name);
-		if (go == null)
-		{
-			Debug.LogError(string.Format("[GUI] Load {0} error in file {1}", typeof(T).FullName, name));
-			return default(T);
-		}
-		var gui = go.GetComponent<T>();
-		if (gui == null)
-		{
-			GameObject.Destroy(go);
-			Debug.LogError(string.Format("[GUI] Can't find component {0} in file {1}", typeof(T).FullName, name));
-			return default(T);
-		}
-
-		guiCache[typeof(T).FullName] = gui;
-		return gui;
-	}
-
-	/// <summary>
-	/// 根据给定的GUI名称加载，不进行任何重复加载判断
-	/// </summary>
-	/// <param name="name"></param>
-	/// <returns>失败返回null</returns>
-	public GameObject LoadGui(string name)
-	{
-		if (name.StartsWith("GX"))
-			name = name.Substring(2);
-		var map = Resources.Load("Prefabs/Gui/" + name);
-		if (map != null)
-		{
-			var go = GameObject.Instantiate(map) as GameObject;
-			if (go != null)
-			{
-				go.transform.parent = uiRoot.transform;
-				go.transform.localScale = Vector3.one;
-				go.transform.position = Vector3.zero;
-				return go;
-			}
-		}
-		return null;
+		return this.GetComponentsDescendant<T>().FirstOrDefault();
 	}
 	#endregion
 
@@ -96,16 +51,11 @@ public class BattleScene : MonoBehaviour
 	}
 	#endregion
 
-	IEnumerator Start()
+	void Start()
 	{
 		if (Instance != null)
 			throw new System.InvalidOperationException();
 		Instance = this;
-		yield return null; // 所有GUI必须在此之后加载，否则编辑器中直接从该场景启动游戏会报错
-		Gui<GXChatInput>();
-		Gui<GXChatOutput>();
-		Gui<GXRoleHead>();
-		LoadGui("ControlBar");
 	}
 
 	void OnDestroy()
