f3818e591495a6509a82fdd44254651f36181438
yangmingzhe <miaodadao@gmail.com>
2014-03-20 14:17:08 +0800

    RoleInfo子界面通过PrefabLoader加载

---
+++ Assets/Scripts/Gui/PrefabLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabLoader : MonoBehaviour
+{
+	public string[] prefabs;
+
+	void Start()
+	{
+		foreach (var p in prefabs)
+		{
+			LoadPrefab(p);
+		}
+	}
+
+	private GameObject LoadPrefab(string path)
+	{
+		var r = Resources.Load(path);
+		if (r == null)
+		{
+			Debug.LogWarning("Can't load prefab: " + path);
+			return null;
+		}
+		var go = GameObject.Instantiate(r) as GameObject;
+		if (go == null)
+		{
+			Debug.LogError("Can't instant GameObject from prefab: " + path);
+			return null;
+		}
+		go.transform.parent = this.transform;
+		go.transform.localEulerAngles = Vector3.one;
+		go.transform.position = Vector3.zero;
+		return go;
+	}
+}
