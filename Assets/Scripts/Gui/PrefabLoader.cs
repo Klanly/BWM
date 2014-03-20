744aade2c7695a08f67c4fc904a82c884bc4e44b
yangmingzhe <miaodadao@gmail.com>
2014-03-20 14:34:10 +0800

    PrefabLoader可直接采用Prefab拖放，比填字符串好

--- Assets/Scripts/Gui/PrefabLoader.cs
+++ Assets/Scripts/Gui/PrefabLoader.cs
@@ -3,33 +3,16 @@ using System.Collections;
 
 public class PrefabLoader : MonoBehaviour
 {
-	public string[] prefabs;
+	public GameObject[] prefabs;
 
 	void Start()
 	{
-		foreach (var p in prefabs)
+		foreach (var pf in prefabs)
 		{
-			LoadPrefab(p);
+			var go = GameObject.Instantiate(pf) as GameObject;
+			go.transform.parent = this.transform;
+			go.transform.localScale = Vector3.one;
+			go.transform.position = Vector3.zero;
 		}
-	}
-
-	private GameObject LoadPrefab(string path)
-	{
-		var r = Resources.Load(path);
-		if (r == null)
-		{
-			Debug.LogWarning("Can't load prefab: " + path);
-			return null;
-		}
-		var go = GameObject.Instantiate(r) as GameObject;
-		if (go == null)
-		{
-			Debug.LogError("Can't instant GameObject from prefab: " + path);
-			return null;
-		}
-		go.transform.parent = this.transform;
-		go.transform.localEulerAngles = Vector3.one;
-		go.transform.position = Vector3.zero;
-		return go;
 	}
 }
