6e29de8629b93010600a6ee243d448f99c7ed418
yunana <yunana@ztgame.com>
2014-03-22 20:33:05 +0800

    技能绑定的特效可以是技能组，也能支持自动删除

---
+++ Assets/Scripts/Skill/ParticleParentAutoDestroy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleParentAutoDestroy : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(GetComponentsInChildren<ParticleSystem>().Length == 0)
+			Destroy(gameObject);
+	}
+}
