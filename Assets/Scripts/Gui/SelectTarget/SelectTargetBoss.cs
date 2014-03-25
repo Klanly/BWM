522e2709b2f40cd50f7940c24585f6fad0a639ca
yangmingzhe <miaodadao@gmail.com>
2014-03-25 23:08:28 +0800

    rename SelectTargetBossView to SelectTargetBoss

---
+++ Assets/Scripts/Gui/SelectTarget/SelectTargetBoss.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectTargetBoss : MonoBehaviour
+{
+	void Start()
+	{
+	}
+
+	internal void Present(Npc target)
+	{
+		throw new System.NotImplementedException();
+	}
+}
