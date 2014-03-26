a27538932ebb046f0859efd5322f0271a379eb33
yangmingzhe <miaodadao@gmail.com>
2014-03-26 16:06:42 +0800

    RoleInfoAvatar可以显示角色了

--- Assets/Scripts/Gui/RoleInfoAvatar.cs
+++ Assets/Scripts/Gui/RoleInfoAvatar.cs
@@ -3,8 +3,25 @@ using System.Collections;
 
 public class RoleInfoAvatar : MonoBehaviour
 {
+	private GameObject avatar;
+
 	void OnEnable()
 	{
 		NGUITools.BringForward(this.gameObject);
+
+		avatar = Avatar.Create(table.TableAvatar.Select(MainRole.ServerInfo.profession, MainRole.ServerInfo.sexman));
+		avatar.name = "RoleInfoAvatar.avatar";
+		avatar.transform.position = new Vector3(-10, 0, 0);
+		avatar.transform.Rotate(0, 180, 0);
+		avatar.transform.localScale = new Vector3(5, 5, 5);
+	}
+
+	void OnDisable()
+	{
+		if (avatar)
+		{
+			GameObject.Destroy(avatar);
+			avatar = null;
+		}
 	}
 }
