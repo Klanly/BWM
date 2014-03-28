3dac937a41d6f7700974215a5fb20b9da3195bdb
yangmingzhe <miaodadao@gmail.com>
2014-03-28 16:47:11 +0800

    技能界面技能图标展示

--- Assets/Scripts/Gui/SkillInfoItem.cs
+++ Assets/Scripts/Gui/SkillInfoItem.cs
@@ -1,9 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SkillInfoItem : MonoBehaviour
 {
 	public UISprite uiIcon;
 	public UILabel uiName;
 	public UISprite uiUpgrade;
+
+	private KeyValuePair<uint, table.TableSkill> m_tableInfo;
+	public KeyValuePair<uint, table.TableSkill> Skill
+	{
+		get { return m_tableInfo; }
+		set
+		{
+			m_tableInfo = value;
+			Present();
+		}
+	}
+
+	private void Present()
+	{
+		var skill = Skill.Value ?? table.TableSkill.First(Skill.Key);
+		uiIcon.spriteName = skill.icon;
+		uiName.text = skill.name;
+		this.gameObject.GetComponent<UIButton>().isEnabled = Skill.Value != null;
+	}
 }
