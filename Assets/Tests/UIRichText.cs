c2d70f5354c26b245e6ba2a8cfca86d2e7c238e2
yangmingzhe <miaodadao@gmail.com>
2014-04-03 12:01:27 +0800

    RichText 连续的文本节点自动合并

--- Assets/Tests/UIRichText.cs
+++ Assets/Tests/UIRichText.cs
@@ -8,6 +8,28 @@ public class UIRichText : MonoBehaviour
 
 	private UIWidget host;
 
+	private UILabel m_lastLabel;
+	private UILabel LastLabel
+	{
+		get
+		{
+			if (m_lastLabel == null)
+			{
+				var item = NGUITools.AddChild(this.gameObject, protoLabel);
+				item.transform.localPosition = new Vector3(0, -host.height, 0);
+				m_lastLabel = item.GetComponent<UILabel>();
+				m_lastLabel.width = host.width;
+				m_lastLabel.height = 0;
+				m_lastLabel.text = string.Empty;
+			}
+			return m_lastLabel;
+		}
+		set
+		{
+			m_lastLabel = value;
+		}
+	}
+
 	void Start()
 	{
 		host = this.GetComponent<UIWidget>();
@@ -16,25 +38,22 @@ public class UIRichText : MonoBehaviour
 
 	public void AddParagraph(string text = null)
 	{
-		var item = NGUITools.AddChild(this.gameObject, protoLabel);
-		item.transform.localPosition = new Vector3(0, -host.height, 0);
-		var c = item.GetComponent<UILabel>();
-		c.width = host.width;
-
+		var lastHeight = LastLabel.localSize.y;
 		if (string.IsNullOrEmpty(text))
 		{
-			c.text = string.Empty;
+			LastLabel.text += "\n";
 		}
 		else
 		{
 			var str = text.Replace("\t", "    "); // 暂不支持tab字符的显示
-			c.text = str;
+			LastLabel.text += str + "\n";
 		}
-		host.height += (int)c.localSize.y;
+		host.height += (int)(LastLabel.localSize.y - lastHeight);
 	}
 
 	public void AddWidget(GameObject widget)
 	{
+		LastLabel = null;
 		widget.transform.parent = this.transform;
 		widget.layer = this.gameObject.layer;
 	}
