6bef7c757e6e452637d3309c13bc3fcaaaba9ac1
yunana <yunana@ztgame.com>
2014-04-04 17:25:26 +0800

    播放开始特效增加延时

--- Assets/Scripts/Skill/SkillPlayStartParticle.cs
+++ Assets/Scripts/Skill/SkillPlayStartParticle.cs
@@ -4,32 +4,51 @@ using System.Collections;
 [RequireComponent(typeof(Skill))]
 public class SkillPlayStartParticle : SkillBase
 {
-
+	public float delay;
 	public GameObject particle;
 	public string mountOfStartGo;
 
 	override public void StartSkill()
+	{
+		if (delay > 0.0f)
+		{
+			iTween.ValueTo(gameObject, iTween.Hash(
+				"from", delay, 
+				"to", 0.0f, 
+				"time", delay, 
+				"onupdate", "onUpdate",
+				"oncomplete", "PlayParticle"));
+		}
+		else
+		{
+			PlayParticle();
+		}
+	}
+
+	void onUpdate(float delay) { }
+
+	void PlayParticle()
 	{
 		if (particle == null)
 		{
 			Destroy(this);
 			return;
 		}
-
+		
 		var skill = gameObject.GetComponent<Skill>();
 		if (skill && skill.startGo)
 		{
 			var mount = SkillBase.Find(skill.startGo.transform, mountOfStartGo);
 			if (!mount)
 				mount = skill.startGo.transform;
-
+			
 			var particleGo = Instantiate(particle) as GameObject;
 			if(particleGo.GetComponent<ParticleParentAutoDestroy>() == null)
 				particleGo.AddComponent<ParticleParentAutoDestroy>();
 			particleGo.transform.parent = mount;
 			particleGo.transform.localPosition = Vector3.zero;
 		}
-
+		
 		Destroy(this);
 	}
 }
