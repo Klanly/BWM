835f63be551ac958a38011543f7bfbc2b7be31be
yangmingzhe <miaodadao@gmail.com>
2014-04-09 17:10:11 +0800

    RichText UIXmlRichText支持sub/sup文字修饰

--- Assets/Scripts/Gui/UIXmlRichText.cs
+++ Assets/Scripts/Gui/UIXmlRichText.cs
@@ -12,14 +12,22 @@ using System.Xml.Linq;
 ///	文字：
 ///		<n>text node</n>
 ///		simple <n>text node</n> supported
+///		'\t' -> "    "
+///		'\n' -> <br />
 ///	文字修饰：
+///	  TODO: sub和sup因是排版后才加的修饰，会出现多余的空白，应该在排版前就予以考虑
 ///		<b>...</b> 粗体
 ///		<i>...</i> 斜体
 ///		<u>...</u> 下划线
 ///		<s>...</s> 删除线
-/// 段落：段前自动插入缩进，并在有必要时插入换行；段后自动插入换行；其中可以嵌套任意节点
+///		<sup>...<sup> 上标
+///		<sub>...<sub> 下标
+/// 段落：
+///	  段前自动插入缩进，并在有必要时插入换行；段后自动插入换行；其中可以嵌套任意节点
+///	  TODO: p节点的嵌套"<p><p></p></p>"不同于序列"<p></p><p></p>"，应该逐级嵌套缩进
 ///		<p>...</p>
-///	超链接：url为空时将退化为纯文本节点
+///	超链接：
+///	  url为空时将退化为纯文本节点
 ///		<a href="url">text</a> 
 ///	图片：
 ///		<img atlas="atlas path" sprite="sprite name" />
@@ -68,6 +76,8 @@ public class UIXmlRichText : UIRichText
 			case "i":
 			case "u":
 			case "s":
+			case "sub":
+			case "sup":
 				var p = new List<UILabel>();
 				AddXml(e.Nodes(), p);
 				foreach (var n in p)
