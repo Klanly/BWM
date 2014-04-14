35b43bc304d1c91445c696e559ed5a7d9c294798
yangmingzhe <miaodadao@gmail.com>
2014-04-14 17:11:28 +0800

    MiniMap 坐标同步准备

--- Assets/Scripts/Gui/Minimap.cs
+++ Assets/Scripts/Gui/Minimap.cs
@@ -3,28 +3,47 @@ using System.Collections;
 
 public class Minimap : MonoBehaviour
 {
-	public UITexture map;
-	private float m_scale = 1;
-	public float Scale
+	public UITexture mapTexture;
+
+	private float m_extent = 512;
+	public float Extent
 	{
-		get { return m_scale; }
-		set { m_scale = value; Layout(); }
+		get { return m_extent; }
+		set { m_extent = value; Layout(); }
+	}
+
+	public void Setup()
+	{
+		if (MainRole.Instance != null)
+			MainRole.Instance.entity.PositionChanged += Layout;
 	}
 
 	void Start()
 	{
-		MainRole.Instance.entity.PositionChanged += Layout;
+		
 	}
 
 	void OnDestroy()
 	{
-		MainRole.Instance.entity.PositionChanged -= Layout;
+		if (MainRole.Instance != null)
+			MainRole.Instance.entity.PositionChanged -= Layout;
 	}
 
 	private void Layout(Entity obj = null)
 	{
+		if (mapTexture.mainTexture == null)
+			mapTexture.mainTexture = BattleScene.Instance.MapNav.transform.parent.GetComponentInChildren<MapTexture>().texture;
+		var mapNav = BattleScene.Instance.MapNav;
+		var size = new Vector2(mapNav.gridWidth * mapNav.gridXNum, mapNav.gridHeight * mapNav.gridZNum);
+
 		var pos = MainRole.Instance.entity.Position;
-		var map = BattleScene.Instance.MapNav;
-		var size = new Vector2(map.gridWidth * map.gridXNum, map.gridHeight * map.gridZNum);
+
+		//mapTexture.material.mainTextureOffset = new Vector2(
+		//	(pos.x - Extent * 0.5f) / size.x,
+		//	(pos.y - Extent * 0.5f) / size.y);
+		//mapTexture.material.mainTextureScale = new Vector2(
+		//	size.x / Extent,
+		//	size.y / Extent);
+		//mapTexture.SetDirty();
 	}
 }
