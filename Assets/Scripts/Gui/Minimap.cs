2dd3e7e24430ab5bddfa7f500501cf66bd9643ce
yangmingzhe <miaodadao@gmail.com>
2014-04-14 14:59:22 +0800

    MiniMap 调整小地图的Mask图为标准尺寸

--- Assets/Scripts/Gui/Minimap.cs
+++ Assets/Scripts/Gui/Minimap.cs
@@ -3,8 +3,28 @@ using System.Collections;
 
 public class Minimap : MonoBehaviour
 {
+	public UITexture map;
+	private float m_scale = 1;
+	public float Scale
+	{
+		get { return m_scale; }
+		set { m_scale = value; Layout(); }
+	}
+
 	void Start()
 	{
+		MainRole.Instance.entity.PositionChanged += Layout;
+	}
 
+	void OnDestroy()
+	{
+		MainRole.Instance.entity.PositionChanged -= Layout;
+	}
+
+	private void Layout(Entity obj = null)
+	{
+		var pos = MainRole.Instance.entity.Position;
+		var map = BattleScene.Instance.MapNav;
+		var size = new Vector2(map.gridWidth * map.gridXNum, map.gridHeight * map.gridZNum);
 	}
 }
