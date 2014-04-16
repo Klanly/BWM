b0704d314b9906c6a8381c6079ae9745b8395bfc
yangmingzhe <miaodadao@gmail.com>
2014-04-16 14:58:04 +0800

    任务追踪 界面

---
+++ Assets/Scripts/Gui/QuestTrack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestTrack : MonoBehaviour
+{
+	public UIXmlRichText uiXmlRichText;
+
+	// Use this for initialization
+	IEnumerator Start()
+	{
+		yield return new WaitForEndOfFrame();
+		uiXmlRichText.AddXml("测试测试测试\n");
+		uiXmlRichText.AddXml("测试测试测试\n");
+		uiXmlRichText.AddXml("测试测试测试\n");
+		uiXmlRichText.AddXml("测试测试测试\n");
+		uiXmlRichText.AddXml("测试测试测试");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
