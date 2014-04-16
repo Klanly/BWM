c81ad7d2b969b94dc7397edc95fdf7d4641662ff
yangmingzhe <miaodadao@gmail.com>
2014-04-16 18:04:23 +0800

    MiniMap 小地图初始显示为透明

--- Assets/Scripts/Gui/Minimap.cs
+++ Assets/Scripts/Gui/Minimap.cs
@@ -16,9 +16,12 @@ public class Minimap : MonoBehaviour
 
 	public void Setup()
 	{
+		mapTexture.mainTexture = BattleScene.Instance.MapNav.transform.parent.GetComponentInChildren<MapTexture>().texture;
+		mapTexture.gameObject.SetActive(mapTexture.mainTexture != null);
+
 		if (MainRole.Instance != null)
 			MainRole.Instance.entity.PositionChanged += OnMainRolePositionChanged;
-		OnMainRolePositionChanged(MainRole.Instance.entity);		
+		OnMainRolePositionChanged(MainRole.Instance.entity);
 	}
 
 	void OnDestroy()
@@ -34,17 +37,16 @@ public class Minimap : MonoBehaviour
 
 	void Update()
 	{
-		if (Layout)
+		if (Layout && mapTexture.gameObject.activeSelf)
 		{
 			Layout = false;
-			if (mapTexture.mainTexture == null)
-				mapTexture.mainTexture = BattleScene.Instance.MapNav.transform.parent.GetComponentInChildren<MapTexture>().texture;
 			var mapNav = BattleScene.Instance.MapNav;
 			var size = new Vector2(mapNav.gridWidth * mapNav.gridXNum, mapNav.gridHeight * mapNav.gridZNum);
 
 			var pos = MainRole.Instance.entity.Position;
 
 			var material = mapTexture.material;
+			material.SetFloat("_Cutoff", mapTexture.mainTexture == null ? 0 : 0.1f);
 			material.mainTextureOffset = new Vector2(
 				Mathf.Clamp((pos.x - Extent / mapTexture.mainTexture.width * 0.5f) / size.x, 0, 1 - Extent / mapTexture.mainTexture.width),
 				Mathf.Clamp((pos.y - Extent / mapTexture.mainTexture.height * 0.5f) / size.y, 0, 1 - Extent / mapTexture.mainTexture.height));
