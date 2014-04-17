3ec53987711e38b5f95a623287db5fc0e6234095
yangmingzhe <miaodadao@gmail.com>
2014-04-17 11:50:05 +0800

    任务 任务界面可正常显隐

--- Assets/Scripts/Gui/RoleInfo.cs
+++ Assets/Scripts/Gui/RoleInfo.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class RoleInfo : MonoBehaviour
 {
+	public UIButton uiQuest;
+
+	void Start()
+	{
+		UIEventListener.Get(uiQuest.gameObject).onClick = go => BattleScene.Instance.Gui<QuestDialog>().gameObject.SetActive(true);
+	}
 	void OnEnable()
 	{
 		NGUITools.BringForward(this.gameObject);
