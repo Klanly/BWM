dd6737b2d9e62679ec7cf23594d816b9cfbd6d5e
yangmingzhe <miaodadao@gmail.com>
2014-04-17 10:43:50 +0800

    任务 任务界面脚本挂接

---
+++ Assets/Scripts/Gui/QuestDialog.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cmd;
+using GX.Net;
+
+public class QuestDialog : MonoBehaviour
+{
+	public const string UriSchemeIndex = "index://";
+
+	public UILabel uiTitle;
+	public UIScrollView uiScrollView;
+	public UIXmlRichText uiXmlRichText;
+
+	public ulong tempid { get; private set; }
+	public string token { get; private set; }
+
+	void Start()
+	{
+		uiXmlRichText.UrlClicked += OnUrlClicked;
+	}
+
+	void OnEnable()
+	{
+		NGUITools.BringForward(this.gameObject);
+	}
+
+	public void SetTitle(string title)
+	{
+		uiTitle.text = title;
+	}
+
+	public void SetMessage(string xml)
+	{
+		uiXmlRichText.Clear();
+		uiXmlRichText.AddXml(xml);
+	}
+
+	private void OnUrlClicked(UIWidget sender, string url)
+	{
+		Debug.Log(string.Format("OnUrlClicked: {0}, {1}", sender.name, url));
+	}
+}
