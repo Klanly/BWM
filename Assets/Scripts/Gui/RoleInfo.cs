f93405b0a6d35b9ccf8b197a3fc18b57a725f675
yangmingzhe <miaodadao@gmail.com>
2014-04-18 16:01:13 +0800

    任务 角色信息界面不再提供任务交接界面入口

--- Assets/Scripts/Gui/RoleInfo.cs
+++ Assets/Scripts/Gui/RoleInfo.cs
@@ -3,12 +3,6 @@ using System.Collections;
 
 public class RoleInfo : MonoBehaviour
 {
-	public UIButton uiQuest;
-
-	void Start()
-	{
-		UIEventListener.Get(uiQuest.gameObject).onClick = go => BattleScene.Instance.Gui<QuestDialog>().gameObject.SetActive(true);
-	}
 	void OnEnable()
 	{
 		NGUITools.BringForward(this.gameObject);
