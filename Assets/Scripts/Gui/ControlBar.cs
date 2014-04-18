59447153ee3d08f200a41f907413a08f60b2fe1b
yangmingzhe <miaodadao@gmail.com>
2014-04-18 16:07:19 +0800

    技能界面通过角色界面中的按钮打开

--- Assets/Scripts/Gui/ControlBar.cs
+++ Assets/Scripts/Gui/ControlBar.cs
@@ -5,7 +5,6 @@ using System.Linq;
 public class ControlBar : MonoBehaviour
 {
 	public UIButton roleInfoButton;
-	public UIButton skillInfoButton;
 	public UIButton[] uiSkillFireThumbs;
 
 	void Start()
@@ -14,13 +13,6 @@ public class ControlBar : MonoBehaviour
 		UIEventListener.Get(roleInfoButton.gameObject).onClick = go =>
 		{
 			var target = BattleScene.Instance.Gui<RoleInfo>();
-			target.gameObject.SetActive(!target.gameObject.activeSelf);
-		};
-
-		// 切换SkillInfo界面的显隐
-		UIEventListener.Get(skillInfoButton.gameObject).onClick = go =>
-		{
-			var target = BattleScene.Instance.Gui<SkillInfo>();
 			target.gameObject.SetActive(!target.gameObject.activeSelf);
 		};
 
--- Assets/Scripts/Gui/RoleInfo.cs
+++ Assets/Scripts/Gui/RoleInfo.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class RoleInfo : MonoBehaviour
 {
+	public UIButton uiSkillInfo;
+
+	void Start()
+	{
+		UIEventListener.Get(uiSkillInfo.gameObject).onClick = go =>
+		{
+			var target = BattleScene.Instance.Gui<SkillInfo>();
+			target.gameObject.SetActive(!target.gameObject.activeSelf);
+		};
+	}
 	void OnEnable()
 	{
 		NGUITools.BringForward(this.gameObject);
