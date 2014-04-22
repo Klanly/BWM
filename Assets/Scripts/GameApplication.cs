d8fb905d10f5a6273afa313a17910b0d90b8a85c
yangmingzhe <miaodadao@gmail.com>
2014-04-22 16:21:32 +0800

    GameStateChanged stub

---
+++ Assets/Scripts/GameApplication.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public delegate void ValueChanged<T>(T last, T cur);
+
+public enum GameState
+{
+	None,
+}
+
+public static class GameApplication
+{
+	private static GameState gameState;
+	public static GameState GameState
+	{
+		get { return gameState; }
+		set
+		{
+			if (gameState == value)
+				return;
+			var last = gameState;
+			gameState = value;
+			if (GameStateChanged != null)
+				GameStateChanged(last, gameState);
+		}
+	}
+	public static event ValueChanged<GameState> GameStateChanged;
+}
