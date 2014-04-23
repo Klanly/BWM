ee851c148bcf3cf30f398935d8184d7c66f6e2c7
yangmingzhe <miaodadao@gmail.com>
2014-04-23 18:25:30 +0800

    小地图 主角图标位置代码整理

--- Assets/Scripts/Gui/Minimap.cs
+++ Assets/Scripts/Gui/Minimap.cs
@@ -5,6 +5,8 @@ public class Minimap : MonoBehaviour
 {
 	public UITexture uiMapTexture;
 	public UISprite uiFlagMainRole;
+	public UISprite uiFlagRole;
+	public UISprite[] uiFlagNpc;
 
 	private float m_extent = 1024;
 	public float Extent
@@ -53,9 +55,9 @@ public class Minimap : MonoBehaviour
 			var mapNav = BattleScene.Instance.MapNav;
 			var size = new Vector2(mapNav.gridWidth * mapNav.gridXNum, mapNav.gridHeight * mapNav.gridZNum);
 			Extent = Mathf.Min(Extent, uiMapTexture.mainTexture.width, uiMapTexture.mainTexture.height);
+			var relativeExtent = new Vector2(Extent / (float)uiMapTexture.mainTexture.width, Extent / (float)uiMapTexture.mainTexture.height);
 
 			var relativePos = new Vector2(MainRole.Instance.entity.Position.x / size.x, MainRole.Instance.entity.Position.z / size.y);
-			var relativeExtent = new Vector2(Extent / (float)uiMapTexture.mainTexture.width, Extent / (float)uiMapTexture.mainTexture.height);
 
 			// 地图位置更新
 			material.mainTextureOffset = new Vector2(
@@ -71,14 +73,7 @@ public class Minimap : MonoBehaviour
 			}
 
 			// 主角图标位置更新
-			var p = relativePos - material.mainTextureOffset;
-			p.x /= relativeExtent.x;
-			p.y /= relativeExtent.y;
-			uiFlagMainRole.leftAnchor.Set(p.x, -uiFlagMainRole.width * 0.5f);
-			uiFlagMainRole.rightAnchor.Set(p.x, uiFlagMainRole.width * 0.5f);
-			uiFlagMainRole.topAnchor.Set(p.y, uiFlagMainRole.height * 0.5f);
-			uiFlagMainRole.bottomAnchor.Set(p.y, -uiFlagMainRole.height * 0.5f);
-			uiFlagMainRole.UpdateAnchors();
+			LayoutFlag(MainRole.Instance.entity, uiFlagMainRole);
 		}
 
 		// 主角图标显隐
@@ -86,5 +81,31 @@ public class Minimap : MonoBehaviour
 		// 主角图标旋转
 		if (MainRole.Instance != null)
 			uiFlagMainRole.transform.localRotation = Quaternion.Euler(0, 180, MainRole.Instance.transform.localRotation.eulerAngles.y);
+	}
+
+	void LayoutFlag(Entity entity, UISprite flag)
+	{
+		var mapNav = BattleScene.Instance.MapNav;
+		var size = new Vector2(mapNav.gridWidth * mapNav.gridXNum, mapNav.gridHeight * mapNav.gridZNum);
+		var relativeExtent = new Vector2(Extent / (float)uiMapTexture.mainTexture.width, Extent / (float)uiMapTexture.mainTexture.height);
+
+		var relativePos = new Vector2(entity.Position.x / size.x, entity.Position.z / size.y);
+		var p = relativePos - material.mainTextureOffset;
+		p.x /= relativeExtent.x;
+		p.y /= relativeExtent.y;
+		uiFlagMainRole.leftAnchor.Set(p.x, -uiFlagMainRole.width * 0.5f);
+		uiFlagMainRole.rightAnchor.Set(p.x, uiFlagMainRole.width * 0.5f);
+		uiFlagMainRole.topAnchor.Set(p.y, uiFlagMainRole.height * 0.5f);
+		uiFlagMainRole.bottomAnchor.Set(p.y, -uiFlagMainRole.height * 0.5f);
+		uiFlagMainRole.UpdateAnchors();
+	}
+
+
+	UISprite GetNpcFlag(Npc npc)
+	{
+		var index = (int)npc.TableInfo.BaseType;
+		if(index >= 0 && index < uiFlagNpc.Length)
+			return uiFlagNpc[index];
+		return null;
 	}
 }
