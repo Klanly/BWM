49f3d31d5f0f654c2df292cdd0c1299d85866942
yangmingzhe <miaodadao@gmail.com>
2014-04-23 20:39:37 +0800

    小地图 NPC标记位置可显示

--- Assets/Scripts/Gui/Minimap.cs
+++ Assets/Scripts/Gui/Minimap.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Minimap : MonoBehaviour
 {
@@ -7,6 +8,8 @@ public class Minimap : MonoBehaviour
 	public UISprite uiFlagMainRole;
 	public UISprite uiFlagRole;
 	public UISprite[] uiFlagNpc;
+
+	private readonly Dictionary<Entity, UISprite> flags = new Dictionary<Entity, UISprite>();
 
 	private float m_extent = 1024;
 	public float Extent
@@ -34,12 +37,30 @@ public class Minimap : MonoBehaviour
 		// 对material clone一份，防止运行时的修改影响到源文件
 		material = (Material)GameObject.Instantiate(uiMapTexture.material);
 		uiMapTexture.material = material;
+
+		Npc.All.ItemAdd += OnNpcAdd;
+		Npc.All.ItemRemove += OnNpcRemove;
+		Role.All.ItemAdd += OnRoleAdd;
+		Role.All.ItemRemove += OnRoleRemove;
+		foreach (var i in Npc.All)
+			AddFlag(i.Value.GetComponent<Entity>(), GetNpcFlag(i.Value));
+		foreach (var i in Role.All)
+		{
+			if(i.Value.ServerInfo.charid != MainRole.ServerInfo.userdata.charid)
+				AddFlag(i.Value.GetComponent<Entity>(), uiFlagRole);
+		}
 	}
 
 	void OnDestroy()
 	{
 		if (MainRole.Instance != null)
 			MainRole.Instance.entity.PositionChanged -= OnMainRolePositionChanged;
+
+		Npc.All.ItemAdd -= OnNpcAdd;
+		Npc.All.ItemRemove -= OnNpcRemove;
+		Role.All.ItemAdd -= OnRoleAdd;
+		Role.All.ItemRemove -= OnRoleRemove;
+		flags.Clear();
 	}
 
 	void OnMainRolePositionChanged(Entity sender)
@@ -81,9 +102,34 @@ public class Minimap : MonoBehaviour
 		// 主角图标旋转
 		if (MainRole.Instance != null)
 			uiFlagMainRole.transform.localRotation = Quaternion.Euler(0, 180, MainRole.Instance.transform.localRotation.eulerAngles.y);
+
+		foreach (var f in flags)
+			LayoutFlag(f.Key, f.Value);
 	}
 
-	void LayoutFlag(Entity entity, UISprite flag)
+	private bool AddFlag(Entity entity, UISprite proto)
+	{
+		if (entity == null || proto == null)
+			return false;
+		var sprite = NGUITools.AddChild(uiMapTexture.transform.parent.gameObject, proto.gameObject).GetComponent<UISprite>();
+		sprite.name = entity.name;
+		flags.Add(entity, sprite);
+		return true;
+	}
+
+	private bool RemoveFlag(Entity entity)
+	{
+		if (entity == null)
+			return false;
+		UISprite sprite;
+		if (flags.TryGetValue(entity, out sprite) == false)
+			return false;
+		flags.Remove(entity);
+		GameObject.Destroy(sprite.gameObject);
+		return true;
+	}
+
+	private void LayoutFlag(Entity entity, UISprite flag)
 	{
 		var mapNav = BattleScene.Instance.MapNav;
 		var size = new Vector2(mapNav.gridWidth * mapNav.gridXNum, mapNav.gridHeight * mapNav.gridZNum);
@@ -93,19 +139,39 @@ public class Minimap : MonoBehaviour
 		var p = relativePos - material.mainTextureOffset;
 		p.x /= relativeExtent.x;
 		p.y /= relativeExtent.y;
-		uiFlagMainRole.leftAnchor.Set(p.x, -uiFlagMainRole.width * 0.5f);
-		uiFlagMainRole.rightAnchor.Set(p.x, uiFlagMainRole.width * 0.5f);
-		uiFlagMainRole.topAnchor.Set(p.y, uiFlagMainRole.height * 0.5f);
-		uiFlagMainRole.bottomAnchor.Set(p.y, -uiFlagMainRole.height * 0.5f);
-		uiFlagMainRole.UpdateAnchors();
+		flag.leftAnchor.Set(p.x, -flag.width * 0.5f);
+		flag.rightAnchor.Set(p.x, flag.width * 0.5f);
+		flag.topAnchor.Set(p.y, flag.height * 0.5f);
+		flag.bottomAnchor.Set(p.y, -flag.height * 0.5f);
+		flag.UpdateAnchors();
 	}
-
 
 	UISprite GetNpcFlag(Npc npc)
 	{
 		var index = (int)npc.TableInfo.BaseType;
-		if(index >= 0 && index < uiFlagNpc.Length)
+		if (index >= 0 && index < uiFlagNpc.Length)
 			return uiFlagNpc[index];
 		return null;
+	}
+
+	private void OnNpcAdd(object sender, GX.EventArgs<KeyValuePair<ulong, Npc>> args)
+	{
+		AddFlag(args.Data.Value.GetComponent<Entity>(), GetNpcFlag(args.Data.Value));
+	}
+
+	private void OnNpcRemove(object sender, GX.EventArgs<KeyValuePair<ulong, Npc>> args)
+	{
+		RemoveFlag(args.Data.Value.GetComponent<Entity>());
+	}
+
+	private void OnRoleAdd(object sender, GX.EventArgs<KeyValuePair<ulong, Role>> args)
+	{
+		if (args.Data.Value.ServerInfo.charid != MainRole.ServerInfo.userdata.charid)
+			AddFlag(args.Data.Value.GetComponent<Entity>(), uiFlagRole);
+	}
+
+	private void OnRoleRemove(object sender, GX.EventArgs<KeyValuePair<ulong, Role>> args)
+	{
+		RemoveFlag(args.Data.Value.GetComponent<Entity>());
 	}
 }
