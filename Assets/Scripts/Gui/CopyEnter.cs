056ed3c7fedea447b7e04e26ee67cebead3ed2f8
yunana <yunana@ztgame.com>
2014-06-19 09:11:57 +0800

    增加副本按钮的响应

--- Assets/Scripts/Gui/CopyEnter.cs
+++ Assets/Scripts/Gui/CopyEnter.cs
@@ -4,11 +4,37 @@ using System.Collections;
 public class CopyEnter : MonoBehaviour {
 
 	public GameObject uiClose;
+	public GameObject[] uiCopys;
+	public GameObject uiEnter;
+	private int selected = 0;
 
 	// Use this for initialization
 	void Start () 
 	{
 		BattleScene.AddToPanel(gameObject);
+
+		// 关闭按钮
 		UIEventListener.Get(uiClose).onClick = go => Destroy(gameObject);
+
+		// 每个副本按钮点击事件
+		for (int i = 0; i < uiCopys.Length; ++i)
+		{
+			var index = i;
+			UIEventListener.Get(uiCopys[i]).onClick = go =>
+			{
+				Debug.Log("copy" + index);
+				foreach(var t in uiCopys)
+					t.GetComponent<UIButton>().enabled = true;
+				uiCopys[index].GetComponent<UIButton>().enabled = false;
+				selected = index;
+			};
+		}
+
+		// 进入按钮
+		UIEventListener.Get(uiEnter).onClick = go => 
+		{
+			Debug.Log("select copy:" + selected);
+			Destroy(gameObject);
+		};
 	}
 }
