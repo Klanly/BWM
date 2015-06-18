38846f4d738815e154f048e18f18582ca34422ae
yangmingzhe <277894613@qq.com>
2015-06-18 11:40:57 +0800

    摇杆控制角色旋转

---
+++ Assets/Demo/Demo.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour
+{
+	public GXJoystick joystick;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (joystick)
+		{
+			float horizontal = Input.GetAxis("Horizontal");
+			float vertical = Input.GetAxis("Vertical");
+			if (horizontal == 0 && vertical == 0)
+			{
+				horizontal = joystick.position.x;
+				vertical = joystick.position.y;
+			}
+
+			MoveByJoystick(horizontal, vertical, joystick.pressed);
+		}
+	}
+
+	/// <summary>
+	/// 检测摇杆输入控制移动
+	/// </summary>
+	public void MoveByJoystick(float horizontal, float vertical, bool pressed)
+	{
+		Vector3 rootDirection = this.transform.forward;
+		Vector3 stickDirection = new Vector3(horizontal, 0, vertical);
+
+		// Get camera rotation.    
+		Vector3 CameraDirection = Camera.main.transform.forward;
+		CameraDirection.y = 0.0f; // kill Y
+		Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, CameraDirection);
+
+		// Convert joystick input in Worldspace coordinates
+		Vector3 moveDirection = referentialShift * stickDirection;
+		MoveWithDirection(moveDirection);
+	}
+
+	/// <summary>
+	/// 朝指定方向向前移动
+	/// </summary>
+	/// <param name="vecDir">look rotation(= target - position)</param>
+	void MoveWithDirection(Vector3 vecDir)
+	{
+		if (vecDir == Vector3.zero)
+			return;
+		vecDir.Normalize();
+
+		var r = Quaternion.Euler(0, -Mathf.Atan2(vecDir.z, vecDir.x) * Mathf.Rad2Deg + 90, 0);
+		this.transform.rotation = r;
+	}
+}
--- Assets/Scripts/Role/CameraFollow.cs
+++ Assets/Scripts/Role/CameraFollow.cs
@@ -3,14 +3,6 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour
 {
-
-	public float distanceCameraToRole = 25.0f;
-	public float heightCameraLookAt = 0.5f;
-
-	private Camera cameraMain;
-	private MapNav MapNav { get { return BattleScene.Instance.MapNav; } }
-
-
 	// Use this for initialization
 	void Start()
 	{
@@ -22,19 +14,9 @@ public class CameraFollow : MonoBehaviour
 	/// </summary>
 	public void UpdateCamera()
 	{
-		if (cameraMain == null)
-			cameraMain = Camera.main;
-
-		var targetCenter = this.transform.position;
-		targetCenter.z += heightCameraLookAt;
-
-		var dx = cameraMain.orthographicSize * Screen.width / Screen.height;
-		var dz = cameraMain.orthographicSize / Mathf.Sin(cameraMain.transform.rotation.eulerAngles.x * Mathf.Deg2Rad) + heightCameraLookAt;
-
-		targetCenter.x = Mathf.Clamp(targetCenter.x, dx, MapNav.transform.localScale.x - dx);
-		targetCenter.z = Mathf.Clamp(targetCenter.z, dz, MapNav.transform.localScale.y - dz);
-
-		var pos = targetCenter + cameraMain.transform.rotation * Vector3.back * distanceCameraToRole;
-		cameraMain.transform.position = pos;
+		var pos = this.transform.position;
+		pos.y += 2.1f;
+		pos.z += -2.1f;
+		Camera.main.transform.position = pos;
 	}
 }
