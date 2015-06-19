9026bca2a9e640dfe39f3adb7c71cfa529469d85
yangmingzhe <277894613@qq.com>
2015-06-19 16:51:06 +0800

    鼠标拖动可实现摄像机移动

---
+++ Assets/Demo/SceneInput.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cmd;
+using GX.Net;
+
+/// <summary>
+/// 战斗场景输入处理
+/// </summary>
+/// <remarks>
+/// NGUI: Events
+/// ref: http://www.tasharen.com/?page_id=160
+/// </remarks>
+public class SceneInput : MonoBehaviour
+{
+	public CameraFollow cameraFollow;
+
+	private bool pressing = false;
+
+	void Start()
+	{
+		UICamera.fallThrough = this.gameObject;
+	}
+
+	void Destroy()
+	{
+		UICamera.fallThrough = null;
+	}
+
+	/// <summary>
+	/// Sent when a mouse button (or touch event) gets pressed over the collider (with ‘true’)
+	/// and when it gets released (with ‘false’, sent to the same collider even if it’s released elsewhere).
+	/// </summary>
+	/// <param name="isDown"></param>
+	void OnPress(bool isDown)
+	{
+		if (isDown)
+		{
+			var ray = Camera.main.ScreenPointToRay(new Vector3(UICamera.lastTouchPosition.x, UICamera.lastTouchPosition.y));
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				if (SelectTarget.Select(hit.collider.gameObject) == false)
+				{
+					// 没有选中目标，就移动
+					if (hit.collider.gameObject.GetComponent<MapNav>())
+					{
+					}
+				}
+			}
+		}
+
+		pressing = isDown;
+	}
+
+	void Update()
+	{
+		// Esc关闭界面或退出场景
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+
+			var topmost = Closeable.TopMost();
+			if (topmost != null)
+			{
+				topmost.gameObject.SetActive(false);
+			}
+			else if (SelectTarget.Selected != null)
+			{
+				SelectTarget.Selected = null;
+			}
+			else
+			{
+				if (WebSocketClient.Instance.Send(new CharactorUnregSelectUserCmd_CS() { }) == false)
+				{
+					Application.LoadLevel("RoleListScene");
+				}
+			}
+		}
+	}
+	[Execute]
+	public static void Execute(CharactorUnregSelectUserCmd_CS cmd)
+	{
+		Application.LoadLevel("RoleListScene");
+	}
+	/// <summary>
+	/// is sent when keyboard or controller input is used.
+	/// </summary>
+	/// <param name="key"></param>
+	void OnKey(KeyCode key)
+	{
+	}
+}
--- Assets/Scripts/Role/CameraFollow.cs
+++ Assets/Scripts/Role/CameraFollow.cs
@@ -4,8 +4,17 @@ using System.Collections;
 [ExecuteInEditMode]
 public class CameraFollow : MonoBehaviour
 {
+	public GameObject mainRole;
 	public Vector3 distance = new Vector3(0, 5, -3);
 	public Vector3 rotation = new Vector3(45, 0, 0);
+
+	public float sensitivityX = 8F;
+	public float sensitivityY = 8F;
+
+	float mHdg = 0F;
+	float mPitch = 0F;
+
+
 	// Use this for initialization
 	void Start()
 	{
@@ -17,7 +26,80 @@ public class CameraFollow : MonoBehaviour
 	/// </summary>
 	public void UpdateCamera()
 	{
-		Camera.main.transform.position = this.transform.position + distance;
+		Camera.main.transform.position = mainRole.transform.position + distance;
 		Camera.main.transform.rotation = Quaternion.Euler(rotation);
 	}
+
+
+	/// <summary>
+	/// http://answers.unity3d.com/questions/9217/free-view-camera-script.html
+	/// </summary>
+	void Update()
+	{
+		if (!(Input.GetMouseButton(0) || Input.GetMouseButton(1)))
+			return;
+
+		float deltaX = Input.GetAxis("Mouse X") * sensitivityX;
+		float deltaY = Input.GetAxis("Mouse Y") * sensitivityY;
+
+		if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
+		{
+			Strafe(deltaX);
+			ChangeHeight(deltaY);
+		}
+		else
+		{
+			if (Input.GetMouseButton(0))
+			{
+				MoveForwards(deltaY);
+				ChangeHeading(deltaX);
+			}
+			else if (Input.GetMouseButton(1))
+			{
+				ChangeHeading(deltaX);
+				ChangePitch(-deltaY);
+			}
+		}
+	}
+
+	void MoveForwards(float aVal)
+	{
+		Vector3 fwd = transform.forward;
+		fwd.y = 0;
+		fwd.Normalize();
+		transform.position += aVal * fwd;
+	}
+
+	void Strafe(float aVal)
+	{
+		transform.position += aVal * transform.right;
+	}
+
+	void ChangeHeight(float aVal)
+	{
+		transform.position += aVal * Vector3.up;
+	}
+
+	void ChangeHeading(float aVal)
+	{
+		mHdg += aVal;
+		WrapAngle(ref mHdg);
+		transform.localEulerAngles = new Vector3(mPitch, mHdg, 0);
+	}
+
+	void ChangePitch(float aVal)
+	{
+		mPitch += aVal;
+		WrapAngle(ref mPitch);
+		transform.localEulerAngles = new Vector3(mPitch, mHdg, 0);
+	}
+
+	public static void WrapAngle(ref float angle)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+	}
+
 }
